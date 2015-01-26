12cf6a60bf35feae47e7bd460d67227c55fbbba3
Yusup Ashrap <awklsgrep@gmail.com>
2015-01-27 00:47:49 +0800

    chinese segmentation

--- project.clj
+++ project.clj
@@ -1,12 +1,23 @@
 (defproject seg "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+  :description "chinese,uyghur segmentation"
+  :url "http://github.com/xperian/seg"
   :min-lein-version "2.0.0"
-  :dependencies [[org.clojure/clojure "1.6.0"]
+  :dependencies [
+                 [org.clojure/clojure "1.7.0-alpha4"]
                  [compojure "1.3.1"]
-                 [ring/ring-defaults "0.1.2"]]
-  :plugins [[lein-ring "0.8.13"]]
-  :ring {:handler seg.handler/app}
+                 [ring-mock "0.1.5"]
+                 [ring/ring-defaults "0.1.2"]
+                 [ring "1.3.2"]
+                 [ring/ring-json "0.3.1"]
+                 [com.guokr/stan-cn-nlp "0.0.4"]]
+  :plugins [[lein-ring "0.8.13"]
+            [cider/cider-nrepl "0.9.0-SNAPSHOT"]]
+
+
+  :ring {:handler seg.handler/app
+         :port 4000}
   :profiles
-  {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+  {:dev {:dependencies [
+                        [javax.servlet/servlet-api "2.5"]
+                        [com.guokr/stan-cn-nlp "0.0.4"]
                         [ring-mock "0.1.5"]]}})
--- src/seg/handler.clj
+++ src/seg/handler.clj
@@ -1,11 +1,35 @@
 (ns seg.handler
+  (:use [ring.middleware.json :only [wrap-json-response]]
+        [ring.util.response :only [response redirect content-type]])
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [clojure.string :as str]
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]])
+  (:import com.guokr.nlp.seg.__SEG__))
+
+(defn cn-seg [text]
+  "Chinese segmentation. A lot work could have been done."
+  (.segment com.guokr.nlp.seg.__SEG__/INSTANCE text))
+
+(defn chinese? [text ]
+  "This will check if it is Chinese"
+  (some #(or (<=  0x4E00 (int %) 0x9FFF)) text))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (GET "/" [] "you can post data to cn-seg .")
+  (GET "/test" [] "awesome")
+  (GET "/cn" [] "please use post")
+  (POST "/cn" [] (fn [req]
+                      (let [src (get-in req [:params :src])]
+                        (println req)
+                        (if (or  (str/blank? src) (not (chinese? src)))
+                          (response  {:text ""})
+                          (response {:text (cn-seg src)})
+                          ))))
+  (ANY "*" []
+       (redirect "/")))
 
 (def app
-  (wrap-defaults app-routes site-defaults))
+  (-> app-routes
+      wrap-json-response
+      (wrap-defaults (assoc-in  site-defaults [:security :anti-forgery] false))))
